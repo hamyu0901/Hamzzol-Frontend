bdc590d523eabe23398c28b30844125b26508aec
yujin <yujin@doolim-yaskawa.com>
2023-08-30 17:39:51 +0900

    [HZFront-3] /components/user-container/UserContainer.vue

---
+++ src/components/user-container/UserContainer.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-layout :class="$style['user-container']">
+        <v-layout :class="$style['user-container__layout']" @click="clickLogo">
+            <img alt="Vue logo" src="@/assets/img/logo.png" height="100" width="120">
+            <span :class="$style['user-container__layout-title']">Hamzzol-World</span>
+        </v-layout>
+        <router-view></router-view>
+    </v-layout>
+</template>
+<script lang="ts" setup>
+import router from "@/router";
+
+const clickLogo = () => {
+    router.push({ path: `/` });
+};
+</script>
+<style lang="scss" module>
+@import 'UserContainer';
+</style>
