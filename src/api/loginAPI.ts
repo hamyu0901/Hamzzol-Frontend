d23460a04db5c9767ca18009814099e07b2c9220
yujin <yujin@doolim-yaskawa.com>
2023-08-31 09:30:41 +0900

    [HZFront-3] /api/loginAPI.ts  - patchUserInfoAPI, uploadUserImgAPI, getUserImgAPI 추가

--- src/api/loginAPI.ts
+++ src/api/loginAPI.ts
@@ -1,22 +1,25 @@
 import { axios } from '@/main';
 
 interface IPostUserInfoType {
-    name: string | null
+    name?: string | null
     id: string | null,
     password: string | null,
-    typeId: number | null,
-    typeAnswer: string | null
+    typeId?: number | null,
+    typeAnswer?: string | null,
 }
 
 const url = {
-    getUserInfoURL: 'login',
+    getUserInfoURL: 'login/id',
     getSecurityQATypeURL: 'login/security/type',
     postUserInfoURL: 'login',
-    getCompareUserInfoURL: 'login/compare'
+    getCompareUserInfoURL: 'login/compare',
+    uploadUserInfoUrl: 'login/file/id',
+    getUserInfoUrl: 'login/file/id',
+    patchUserInfoURL: 'login/id'
 }
 
-const getUserInfoAPI = async(): Promise<any> => {
-    return await axios.get(url.getUserInfoURL);
+const getUserInfoAPI = async(id: string): Promise<any> => {
+    return await axios.get(url.getUserInfoURL + `/${id}`);
 };
 
 const getCompareUserAPI = async (params: {id: string, password: string}): Promise<any> => {
@@ -31,4 +34,20 @@ const postUserInfoAPI = async(body: IPostUserInfoType): Promise<any> => {
     return await axios.post(`${url.postUserInfoURL}`, {...body})
 };
 
-export { getUserInfoAPI, getSecurityQATypeInfo, postUserInfoAPI, getCompareUserAPI }
+const patchUserInfoAPI = async(body: IPostUserInfoType): Promise<any> => {
+    return await axios.patch(`${url.patchUserInfoURL}/${body.id}`, {...body})
+};
+
+const uploadUserImgAPI = async (body: {id: string, fileData: any}): Promise<any> => {
+    return await axios.post(`${url.uploadUserInfoUrl}/${body.id}`, body.fileData, {
+        headers: {
+            'Content-Type': 'multipart/form-data' // Set the correct content type
+        }
+    });
+};
+
+const getUserImgAPI = async (id: string): Promise<any> => {
+    return await axios.get(`${url.getUserInfoUrl}/${id}`);
+};
+
+export { getUserInfoAPI, getSecurityQATypeInfo, postUserInfoAPI, getCompareUserAPI, uploadUserImgAPI, getUserImgAPI, patchUserInfoAPI }
