1b3612dec02a098bc0186454171f3636f3a58106
yujin <yujin@doolim-yaskawa.com>
2023-09-06 17:19:19 +0900

    [HZFront-7] /components/main/weather/WeatherForecast.vue  - 날씨 컴포넌트 생성

--- src/components/main/weather/WeatherForecast.vue
+++ src/components/main/weather/WeatherForecast.vue
@@ -1,7 +1,17 @@
 <template>
     <v-card style="width: 100%" :class="$style['weather-forecast']">
-        <div v-for="(weather, index) in weatherData" :key="index">
-            <img />
+        <span style="color: blue;" :class="$style['font-Eb']">Today {{ location }} Weather</span>
+        <div v-for="(weatherItem, index) in weatherData" :key="index" :class="$style['weather-forecast-container']" :style="index !== weatherData.length-1 && 'margin-right: 20px'">
+            <v-layout :class="$style['weather-forecast-container__layout']">
+                <span :class="$style['font-Eb']">{{setHours(weatherItem)}}</span>
+                <div style="width: 100%; text-align: center" class="mt-2 mb-2">
+                    <img :src="setWeatherSrc(weatherItem)" width="64" height="64" alt="weatherImg"/>
+                </div>
+                <v-layout :class="$style['font-H']" style="justify-content: center;">
+                    <span style="font-size: 20px;">{{weatherItem.main['temp']}}</span>
+                    <span style="padding-top: 5px"> °C</span>
+                </v-layout>
+            </v-layout>
         </div>
     </v-card>
 </template>
@@ -9,9 +19,10 @@
 <script lang="ts" setup>
 import { onMounted, ref } from "vue";
 import { axios } from "@/main";
+import { getWeatherAPI } from "@/api/commonAPI";
 const weatherData = ref<any>([]);
-// const sunsetArray = ref<number[]>([]);
-// const sunriseArray = ref<number[]>([]);
+const sunset = ref<number>(0);
+const location = ref<string>('');
 
 onMounted(() => {
     getWeatherForecast();
@@ -19,31 +30,57 @@ onMounted(() => {
 
 const getWeatherForecast = async () => {
     try {
-        const response = await axios.get(
-            'https://api.openweathermap.org/data/2.5/forecast?q=Seoul&cnt=7&appid=71878480cdd4f838da85f3940b699b86&units=metric',
-        );
-        weatherData.value = response.data.list;
-        const sunsetTimestamp = response.data.city.sunset;
-        const sunsetFormatted = formatDate(sunsetTimestamp);
-
-        const sunset = new Date(sunsetFormatted);
-
-        const beforeSunset = [];
-        const afterSunset = [];
-
-        for (const day of response.data.list) {
-            const date = new Date(day.dt_txt);
-
-            if (date < sunset) {
-                beforeSunset.push(day);
-            }else {
-                afterSunset.push(day)
-            }
+        const weatherApiKey = await getWeatherAPI();
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(async function  (position) {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+                const weatherResult = await axios.get(
+                    `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&cnt=7&appid=${weatherApiKey.data}`
+                );
+                weatherData.value = weatherResult.data.list;
+                sunset.value = weatherResult.data.city.sunset;
+                location.value = weatherResult.data.city.name;
+            }, function (error) {
+                console.error("위치 정보를 가져올 수 없습니다.", error);
+            });
+        } else {
+            console.error("지원하지 않는 브라우저입니다.");
         }
-
     } catch (error) {
         console.error('Error fetching weather data', error);
     }
+};
+
+const setHours = (weatherItem: any) => {
+    return formatHours(weatherItem.dt_txt);
+};
+
+const setWeatherSrc = (weatherItem: any) => {
+    const path = checkSunset(weatherItem);
+
+    switch (weatherItem.weather[0].main) {
+        case 'Thunderstorm':
+            return require(`@/assets/img/common/weather/${path}/thunder.png`);
+        case 'Drizzle':
+        case 'Rain':
+            return require(`@/assets/img/common/weather/${path}/rain.png`);
+        case 'Snow':
+            return require(`@/assets/img/common/weather/${path}/snow.png`);
+        case 'Clear':
+            return require(`@/assets/img/common/weather/${path}/clear.png`);
+        case 'Clouds':
+            return require(`@/assets/img/common/weather/${path}/clouds.png`);
+        default:
+            return require(`@/assets/img/common/weather/${path}/mist.png`);
+    }
+};
+
+const checkSunset = (weatherItem: any) => {
+    const sunsetFormatted = formatDate(sunset.value);
+    const time = new Date(weatherItem.dt_txt);
+    const sunsetTime = new Date(sunsetFormatted);
+    return time < sunsetTime ? 'sunrise' : 'sunset';
 };
 
 const formatDate = (time: number) => {
@@ -56,12 +93,25 @@ const formatDate = (time: number) => {
     const seconds = String(date.getSeconds()).padStart(2, '0');
 
     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-}
+};
 
-// const formatTime = (timestamp: number) => {
-//     const date = new Date(timestamp * 1000);
-//     return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-// };
+const formatHours = (dateString: string) => {
+    const date = new Date(dateString);
+    const hours = date.getHours();
+
+    let period = "오전";
+    let formattedHours: number = hours;
+
+    if (hours >= 12) {
+        period = "오후";
+        if (hours > 12) {
+            formattedHours = hours - 12;
+        }
+    }
+    // 시간을 두 자릿수로 포맷
+    const formattedHoursString = String(formattedHours).padStart(2, "0");
+    return `${period} ${formattedHoursString}시`;
+};
 </script>
 
 <style lang="scss" module>
