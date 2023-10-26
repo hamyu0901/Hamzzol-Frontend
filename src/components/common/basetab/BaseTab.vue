aac01c5160b7f17f1e037abf9371dce80f2858cd
yujin <yujin@doolim-yaskawa.com>
2023-10-26 16:14:07 +0900

    [HzFront-4] /components/common/basetab/BaseTab.vue  - BaseTab.vue 화면 구현

---
+++ src/components/common/basetab/BaseTab.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-layout :class="$style['base-tab']" :style="`height: ${props.height}`">
+      <div v-for="item in items" :key="item.id">
+          <v-btn
+              variant="text"
+              :prepend-icon="item.icon"
+              :class="$style['base-tab-item']"
+              :style="`${item.selected && 'border-bottom: 2px solid #684AFF'}`"
+              @click="clickTabItemHandler(item)">
+              {{ item.title}}
+          </v-btn>
+      </div>
+  </v-layout>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from "vue";
+import { ITabsItemType } from "@/interface/common/common"
+
+const props = defineProps({
+    height : {
+        type : String as PropType<string>,
+        default: '80px'
+    },
+    items: {
+        type: Array as PropType<ITabsItemType[]>,
+        required: true
+    }
+});
+
+const emit = defineEmits(['clickTabItem']);
+
+const clickTabItemHandler = (item: ITabsItemType) => {
+    emit('clickTabItem', item)
+};
+</script>
+
+<style lang="scss" module>
+@import "BaseTab";
+</style>
